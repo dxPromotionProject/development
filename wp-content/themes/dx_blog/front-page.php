727d14e9691564716b8519aa7af8b76e18d50552
横山 敬亮 <>
2021-06-23 23:46:13 +0900

    ペアプロ終了後の更新

--- wp-content/themes/dx_blog/front-page.php
+++ wp-content/themes/dx_blog/front-page.php
@@ -2,8 +2,11 @@
 
 <?php get_template_part('includes/header'); ?>
 
+<h1>ここから下は記事投稿一覧表示機能を実装します</h1>
+
 <?php while (have_posts()): the_post(); ?>
     <h2><?php the_title(); ?></h2>
+    <?php the_post_thumbnail(); ?>
     <!-- その記事を単体で表示したい時はaタグのhref属性の中にthe_permalink()を入れて個々の表示を行う -->
     <!-- the_content()の代わりにthe_exerpt()でもOK(投稿の内容を全て表示したくない場合に使用すること) -->
     <?php the_content(); ?>
